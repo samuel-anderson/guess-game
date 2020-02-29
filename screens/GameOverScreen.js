a90d703347d14931cc2e827b60cad7202a7b04b5
Samuel Anderson <sanderson@panosoft.com>
2020-02-29 00:00:54 -0800

    adding local/network images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, Text, StyleSheet, Button, Image} from 'react-native'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const GaveOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is Over!</Text>
-            <Text>Number of rounds: {props.roundsNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <TitleText>The Game is Over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image
+                    fadeDuration={300}
+                    //for local image 
+                    //source={require('../assets/success.png')} 
+
+                    //for web image, copied image address
+                    source={{uri:'https://cdn5.vectorstock.com/i/1000x1000/11/69/game-over-glitch-vector-18841169.jpg'}}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
+            <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="New Game" onPress={props.onRestart} />
         </View>
     )
@@ -17,6 +31,19 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer:{
+        width:300,
+        height:300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor:  'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width:'100%',
+        height:'100%'
     }
 })
 
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,6 +5,7 @@ import Colors from '../constants/colors'
 import Input from '../components/Input'
 import NumberContainer from '../components/NumberContainer'
 import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const StartGameScreen = props => {
 
@@ -51,7 +52,7 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 
                 <Card style={styles.inputContainter}>
                     <BodyText>Select a Number</BodyText>
