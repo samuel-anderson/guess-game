dea224bdfd2dea2a38920e6a8b2175b029775993
Samuel Anderson <sanderson@panosoft.com>
2020-02-28 22:46:52 -0800

    modify game over screen

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ export default function App() {
   const [userNumber, setUserNumber]= useState()
   const [guessRounds, setGuessRounds] = useState(0);
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
     setGuessRounds(0);
@@ -23,7 +28,7 @@ export default function App() {
   if (userNumber && guessRounds <= 0){
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
   } else if(guessRounds > 0){
-    content = <GameOverScreen />
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
   }
   return (
     <View style={styles.screen}>
