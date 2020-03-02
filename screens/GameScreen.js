92105ce285d6522ba7647ec689d252df2115c39d
Samuel Anderson <sanderson@panosoft.com>
2020-03-01 23:24:30 -0800

    adding icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React, {useState, useRef, useEffect} from 'react'
 import {View, Text, StyleSheet, Button, Alert} from 'react-native'
+
+import { Ionicons} from '@expo/vector-icons';
+
 import NumberContainer from '../components/NumberContainer'
 import Card from '../components/Card'
 //userRef survives component re-renders
@@ -61,10 +64,10 @@ const GameScreen = props => {
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonsContainer}>
                 <MainButton onPress={nextGuessHandler.bind(this, 'lower')} >
-                    LOWER
+                    <Ionicons name="md-remove" size={24} color="white"/>
                 </MainButton>
                 <MainButton onPress={nextGuessHandler.bind(this, 'greater')}> 
-                    GREATER
+                    <Ionicons name="md-add" size={24} color="white"/>
                 </MainButton>
             </Card>
         </View>
