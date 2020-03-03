9df00bde67d528119d44f6f16304844f0eaf40d4
Samuel Anderson <sanderson@panosoft.com>
2020-03-02 22:10:58 -0800

    using flex grow

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -76,8 +76,8 @@ const GameScreen = props => {
                     <Ionicons name="md-add" size={24} color="white"/>
                 </MainButton>
             </Card>
-            <View style={styles.list}>
-                <ScrollView>
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
                     {pastGuesses.map((guess,index) => (renderListItem(guess,pastGuesses.length - index)))}
                 </ScrollView>
             </View>
@@ -98,9 +98,14 @@ const styles = StyleSheet.create({
         width: 400,
         maxWidth: '90%'
     },
-    list:{
+    listContainer:{
         width: '80%',
         flex: 1
+    },
+    list:{
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end'
     },
     listItem:{
         borderColor: '#ccc',
@@ -109,7 +114,8 @@ const styles = StyleSheet.create({
         marginVertical: 10,
         backgroundColor: 'white',
         flexDirection: 'row',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        width: '60%'
         
     }
 });
